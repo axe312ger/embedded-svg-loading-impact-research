5ddaf942036674c5eee2f978ae7ad9ce8fa6a668
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-03-10 13:53:19 +0100

    add count impact pages to test first rendering impact

---
+++ src/count-impact.js
@@ -0,0 +1,55 @@
+const { ensureDir } = require('fs-extra')
+
+const readInputImages = require('./utils/read-input-images')
+const prepareImage = require('./utils/prepare-image')
+const generatePrimitives = require('./utils/generate-primitives')
+const encodeSVG = require('./utils/encode-svg')
+const createGridPage = require('./utils/create-grid-page')
+const {
+  preparedDir,
+  optimizedDir,
+  animatedDir,
+  primitiveDir
+} = require('./config')
+
+const primitiveCount = [10, 25, 50, 75, 100, 200, 500, 750, 1000]
+
+async function run () {
+  await ensureDir(preparedDir)
+  await ensureDir(optimizedDir)
+  await ensureDir(animatedDir)
+  await ensureDir(primitiveDir)
+  try {
+    const inputImages = await readInputImages()
+
+    for (const count of primitiveCount) {
+      const images = []
+
+      for (const inputImage of inputImages) {
+        const image = { ...inputImage }
+        console.log(
+          `Processing ${
+            image.original.name
+          } with ${count} rotated triangles (5)`
+        )
+        const primitiveOptions = {
+          blur: count < 20 ? 12 : 0,
+          mode: 5,
+          numberOfPrimitives: count
+        }
+        await prepareImage(image)
+        await generatePrimitives(image, primitiveOptions)
+        image.dataURI = encodeSVG(image.primitive.optimizedSVG)
+        images.push(image)
+      }
+
+      const title = `${count} rotated triangles`
+      const slug = `count-impact-${count}`
+      await createGridPage({ slug, title, images })
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+run()
