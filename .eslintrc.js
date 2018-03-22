b2303fb74e1454229d1eeb05a4cfd45dd0344a41
Benedikt Rötsch <benedikt@contentful.com>
2018-03-22 16:02:12 +0100

    Updates

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: 'standard',
-  rules: {
-    'no-debugger': 0
-  }
-}
--- src/utils/encode-svg.js
+++
@@ -1,5 +0,0 @@
-const svgToMiniDataURI = require('mini-svg-data-uri')
-
-module.exports = function encode (svg) {
-  return svgToMiniDataURI(svg)
-}
